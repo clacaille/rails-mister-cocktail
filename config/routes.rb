2f5186eda5f0437aaab92b2a42066e00037293ee
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-09 15:34:47 +0100

    Doses controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [ :index, :new, :show, :create ]
   root 'cocktails#index'
+  resources :cocktails, only: [ :index, :new, :show, :create ] do
+    resources :doses, only: [ :new, :create ]
+  end
+
+  delete 'doses/:id', to: 'doses#destroy'
 end
