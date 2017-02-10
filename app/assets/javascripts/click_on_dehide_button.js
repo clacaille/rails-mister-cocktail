11e5adec8c481a888b2eb24608f3fb6959aa9988
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-10 17:20:56 +0100

    js on add ingredient done

--- app/assets/javascripts/click_on_dehide_button.js
+++ app/assets/javascripts/click_on_dehide_button.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $('#button-ingredient').click(function(event) {
-    $('.add-ingredient').toggleClass( "hidden" );
+    $('.add-ingredient').toggleClass("hidden");
+    $('html, body').animate({
+      scrollTop: $('#new_dose').offset().top
+    }, 500)
   });
 });
